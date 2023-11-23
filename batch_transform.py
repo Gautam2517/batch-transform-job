edc6e4ba94b120d8faab0a6bb17971e9f942fa8f
gowtam2517 <gowtam.degapudi@ascentt.com>
2023-11-23 15:18:37 +0530

    test

--- batch_transform.py
+++ batch_transform.py
@@ -38,31 +38,31 @@ transform_input = {
 }
 
 # Specify the output configuration
-transform_output = {
-    'S3OutputPath': S3_OUTPUT_PATH,
+#transform_output = {
+ #   'S3OutputPath': S3_OUTPUT_PATH,
 }
 
 # Specify the parameters for the Batch Transform job
-transform_job_params = {
-    'TransformJobName': JOB_NAME,
-    'ModelName': MODEL_NAME,
-    'MaxConcurrentTransforms': 1,
-    'MaxPayloadInMB': 6,
-    'BatchStrategy': 'MultiRecord',
-    'TransformInput': transform_input,
-    'TransformOutput': transform_output,
-    'TransformResources': {
-        'InstanceType': INSTANCE_TYPE,
-        'InstanceCount': 1,
+#transform_job_params = {
+ #   'TransformJobName': JOB_NAME,
+  #  'ModelName': MODEL_NAME,
+   # 'MaxConcurrentTransforms': 1,
+    #'MaxPayloadInMB': 6,
+    #'BatchStrategy': 'MultiRecord',
+    #'TransformInput': transform_input,
+    #'TransformOutput': transform_output,
+    #'TransformResources': {
+     #   'InstanceType': INSTANCE_TYPE,
+      #  'InstanceCount': 1,
     }
 }
 
 # Create the Batch Transform job
-sagemaker = boto3.client('sagemaker')
-sagemaker.create_transform_job(**transform_job_params)
+#sagemaker = boto3.client('sagemaker')
+#sagemaker.create_transform_job(**transform_job_params)
 
 # Customize the success message or add additional logic here
-print(f"Batch Transform job '{JOB_NAME}' created successfully.")
+#print(f"Batch Transform job '{JOB_NAME}' created successfully.")
 # S3_OUTPUT_PATH = os.getenv("S3_OUTPUT_PATH")
 # MODEL_NAME = os.getenv("MODEL_NAME")
 # INSTANCE_TYPE = os.getenv("INSTANCE_TYPE")
